bbd87b55dcda5e13583ef2b384bd7aaddfa9bec2
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2011-07-02 15:53:07 -0400

    Draft

---
+++ redecard-lib/RedeCard.class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Project:  RedeCard Client Webservice
+ * File:     RedeCard.class.php
+ *
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ *
+ * If you would like to collaborate by suggesting code and documentation enhancements
+ * then please check out readme.txt
+ *  
+ * @file This is the main Class
+ * @copyright 2011 Drupalista.com.br 
+ * @author Francisco Luz <franciscoferreiraluz at yahoo dot com dot au>
+ * @package RedeCard Client Webservice
+ * @version 1.0 Alpha
+ *
+ *  --------------------------------H I R I N G   M E ---------------------------------------------------
+ *  
+ * - I am available for freelancing jobs and casual employment.
+ *   (I speak both english and portuguese fluently, portuguese being my native tongue)
+ * - My services include, but not limited to, training sessions on Drupal for business enterprises,
+ *   individuals and professionals in the IT / Webdevelopment industry;
+ *    # Drupal consulting and module development;
+ *    # Deploying and implementing solutions like:
+ *        1. Open Public (http://openpublicapp.com), ideal for local goverment websites and
+ *           goverment agencies in general.
+ *        2. Open Publish (http://openpublishapp.com), this is a perfect fit for media websites such as
+ *           Tv Networks, News and Magazines.
+ *
+ *   Contact me via
+ *   E-mail: contato at drupalista dot com dot br
+ *   Phone: +55 66 3521 9132 (Business hours, my time zone is Cuiaba, Brazil)
+ *   Mobile: +55 66 9245 5809
+ *
+ *   Francisco Luz
+ *   July 2011
+ */
+class RedeCard {
+    
+    
+    
+}
+?>
